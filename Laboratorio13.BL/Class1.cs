612228b054da1dd0931405fbe4e704e6ec29c954
USER <USER@DESKTOP-PGV89GD>
2023-11-12 19:02:37 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio13.BL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio13.BL
+{
+    public class Class1
+    {
+    }
+}
